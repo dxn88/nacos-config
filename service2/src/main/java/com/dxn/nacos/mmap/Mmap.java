c49ce24955dc2423cfb54914491fd45031ea5f34
dxn <376107773@qq.com>
2021-02-19 17:47:53 +0800

    add: mmap note

---
+++ service2/src/main/java/com/dxn/nacos/mmap/Mmap.java
@@ -0,0 +1,12 @@
+package com.dxn.nacos.mmap;
+
+/**
+ * @author Dai.xn
+ * @since 2021/2/19
+ */
+
+/**
+ * https://blog.csdn.net/qq_36675830/article/details/79283113  Mmap 原理过程
+ */
+public class Mmap {
+}
